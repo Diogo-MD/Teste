0ed25e12d8853a5b159cf14a9342bc6881e3fcad
DIOGO MADEIRA DOS SANTOS <diogo.msantos7@senacsp.edu.br>
2024-04-24 20:22:11 -0300

    Model's

--- 19/model/Pedido.php
+++ 19/model/Pedido.php
@@ -1,4 +1,16 @@
 <?php
 
+class Pedido{
+    private $id;
+    private $data_pedido;
+    private $status;
+
+    public function __construct($id, $data_pedido, $status)
+    {
+        $this->id = $id;
+        $this->data_pedido = $data_pedido;
+        $this->status = $status;
+    }
+}
 
 ?>
--- 19/model/Produto.php
+++ 19/model/Produto.php
@@ -1,4 +1,18 @@
 <?php
 
+class Produto {
+    private $id;
+    private $nome;
+    private $descricao;
+    private $preco;
+
+    public function __construct($id, $nome, $descricao, $preco)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->descricao = $descricao;
+        $this->preco = $preco;
+    }
+}
 
 ?>
