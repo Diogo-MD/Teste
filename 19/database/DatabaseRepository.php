40a6015d0d90af9a53f6c619ba627fd58eac6f04
DIOGO MADEIRA DOS SANTOS <diogo.msantos7@senacsp.edu.br>
2024-04-24 20:28:21 -0300

    Aula 19

---
+++ 19/database/DatabaseRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+class DatabaseRepository {
+    
+    private static $host = 'localhost';
+    private static $username = 'root';
+    private static $password = '';
+    private static $database = 'loja';
+    private static $conn;
+
+    public static function connect() {
+        
+    }
+
+}
+
+?>
